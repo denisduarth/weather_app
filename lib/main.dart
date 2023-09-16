8ca89553f54b6d9f3247f161b34722e5489dfb32
Igor Ribeiro <rimottie@gmail.com>
2023-09-16 10:12:13 -0300

    Criando style.dart, classe ForecastDay e ListView

---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, sort_child_properties_last, sized_box_for_whitespace, must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'style.dart';
+
+void main() => runApp(WheaterApp());
+
+//Classe WeatherApp
+class WheaterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Color(0xff255AF4),
+            body: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  Text("São José do Rio Preto", style: titleStyle),
+                  
+                  Container(
+                    height: 234,
+                    margin: EdgeInsets.only(top: 53),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Image.asset('images/01_sunny_color.png'),
+                          width: 96,
+                          height: 96,
+                        ),
+                        Text("Ensolarado", style: titleStyle),
+                        Text("33°", style: degreeStyle33),
+                      ],
+                    ),
+                  ),
+                  
+                  Container(
+                    height: 70,
+                    margin: EdgeInsets.only(top: 71),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset('images/humidity.png', height: 30, width: 30,),
+                            Text("HUMIDITY", style: textTemperatureStyle),
+                            Text("52%", style: textTemperatureStyle),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children:[
+                            Image.asset('images/wind.png', height: 30, width: 30,),
+                            Text("WIND", style: textTemperatureStyle),
+                            Text("19km/h", style: textTemperatureStyle),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset('images/feels_like.png', height: 30, width: 30,),
+                            Text("FEELS LIKE", style: textTemperatureStyle),
+                            Text("24°", style: textTemperatureStyle),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                  Container(
+                    height: 100,
+                    margin: EdgeInsets.only(top: 98),
+                    child: Expanded(
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          ForecastDay('10AM', '03_cloud_color', 11.7),
+                          ForecastDay('11AM', '04_sun_cloudy_color', 17),
+                          ForecastDay('Now', '01_sunny_color', 33),
+                          ForecastDay('1PM', '04_sun_cloudy_color', 23.4),
+                          ForecastDay('2PM', '11_heavy_rain_color', 18),
+                          ForecastDay('3PM', '03_cloud_color', 15),
+                          ForecastDay('Now', '03_cloud_color', 11.7),
+                          ForecastDay('Now', '03_cloud_color', 11.7),
+                          ForecastDay('Now', '03_cloud_color', 11.7),
+                          ForecastDay('Now', '03_cloud_color', 11.7),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )));
+  }
+}
+
+class ForecastDay extends StatelessWidget {
+  String hour;
+  String image;
+  double temperature;
+
+  ForecastDay(this.hour, this.image, this.temperature);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // height: 100,
+      margin: EdgeInsets.only(left: 39),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                hour,
+                style: topTextStyle,
+              ),
+              Image.asset(
+                'images/$image.png',
+                height: 36,
+                width: 36,
+              ),
+              Text(
+                '$temperature°',
+                style: bottomTextStyle,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/style.dart
@@ -0,0 +1,44 @@
+//Variavéis de estilo
+
+import 'package:flutter/material.dart';
+
+const titleStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 36,
+);
+
+const degreeStyle33 = TextStyle(
+  color: Colors.white,
+  fontSize: 64,
+  fontWeight: FontWeight.w500,
+  shadows: [
+    Shadow(
+        color: Color.fromRGBO(0, 0, 0, 0.25),
+        offset: Offset(4, 4),
+        blurRadius: 2),
+  ],
+);
+
+const textTemperatureStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  shadows: [
+    Shadow(
+        color: Color.fromRGBO(0, 0, 0, 0.25),
+        offset: Offset(4, 4),
+        blurRadius: 2),
+  ],
+);
+
+const bottomTextStyle = TextStyle(
+  fontWeight: FontWeight.w600,
+  fontSize: 20,
+  color: Colors.white,
+);
+
+const topTextStyle = TextStyle(
+  fontSize: 16,
+  color: Colors.white,
+  fontWeight: FontWeight.w400,
+);
